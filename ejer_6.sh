70296ed6361e64af08ff935924e488d884b2f408
Diana Martin Gavilan <diana.gavilan@gmail.com>
2015-11-20 11:57:31 +0100

    Maximun number of engines in Airbus aircrafts

--- ejer_6.sh
+++ ejer_6.sh
@@ -1,5 +1,6 @@
  #!/bin/sh
 csvsort -d '^' -r -c nb_engines optd_aircraft.csv |head -2 |tail -1 
+## extraigo los aviones de airbus y lo guardo en un fichero temporal
 csvgrep -d '^' -c manufacturer  -m Airbus optd_aircraft.csv > temporal.csv
 ## extraigo el mayor numero de motores de un airbus y lo guardo en una variable
 x=$(csvgrep -d '^' -c manufacturer  -m Airbus optd_aircraft.csv |csvcut -c nb_engines |sort -r|uniq | head  -n+2 |tail -n+2)
